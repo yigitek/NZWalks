b2c67d3f27d1a03d50ddaad71e09531dfd870994
yigitek <yigitek@gmail.com>
2023-03-10 13:45:15 +0100

    End of Section4

--- NZWalks/NZWalks/Controllers/RegionsController.cs
+++ NZWalks/NZWalks/Controllers/RegionsController.cs
@@ -20,11 +20,109 @@ namespace NZWalks.API.Controllers
 
 
         [HttpGet]
-        public async Task<IActionResult> GetAllRegions()
+        public async Task<IActionResult> GetAllRegionsAsync()
         {
             var regions = await regionRepository.GetAllAsync();
             var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
             return Ok(regionsDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetRegionAsync")]
+        public async Task<IActionResult> GetRegionAsync(Guid id)
+        {
+            var region = await regionRepository.GetAsync(id);
+            if (region == null)
+                return NotFound();
+
+            var regionDTO = mapper.Map<Models.DTO.Region>(region);
+            return Ok(regionDTO);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRegionAsync(Models.DTO.AddRegionRequest addRegionRequest)
+        {
+            var region = new Models.Domain.Region()
+            {
+                Code = addRegionRequest.Code,
+                Name = addRegionRequest.Name,
+                Area = addRegionRequest.Area,
+                Lat = addRegionRequest.Lat,
+                Long = addRegionRequest.Long,
+                Population = addRegionRequest.Population
+            };
+
+            region = await regionRepository.AddAsync(region);
+
+            var regionDTO = new Models.DTO.Region
+            {
+                Code = region.Code,
+                Name = region.Name,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population,
+                Id = region.Id
+            };
+
+            return CreatedAtAction(nameof(GetRegionAsync), new { id = regionDTO.Id }, regionDTO);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteRegionAsync(Guid id)
+        {
+            var region = await regionRepository.DeleteAsync(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            var regionDTO = new Models.DTO.Region
+            {
+                Code = region.Code,
+                Name = region.Name,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population,
+                Id = region.Id
+            };
+
+            return Ok(regionDTO);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+
+        public async Task<IActionResult> UpdateRegionAsync([FromRoute] Guid id, 
+            [FromBody] Models.DTO.UpdateRegionRequest updateRegionRequest)
+        {
+            var regionToUpdate = new Models.Domain.Region
+            {
+                Code = updateRegionRequest.Code,
+                Name = updateRegionRequest.Name,
+                Area = updateRegionRequest.Area,
+                Lat = updateRegionRequest.Lat,
+                Long = updateRegionRequest.Long,
+                Population = updateRegionRequest.Population
+            };
+            
+            var region = await regionRepository.UpdateAsync(id, regionToUpdate);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            var regionDTO = new Models.DTO.Region
+            {
+                Id= region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population
+            };
+            return Ok(regionDTO);
         }
     }
 }
--- NZWalks/NZWalks/Repositories/IRegionRepository.cs
+++ NZWalks/NZWalks/Repositories/IRegionRepository.cs
@@ -5,5 +5,11 @@ namespace NZWalks.API.Repositories
     public interface IRegionRepository
     {
         Task<IEnumerable<Region>> GetAllAsync();
+
+        Task<Region> GetAsync(Guid Id);
+        Task<Region> AddAsync(Region region);
+
+        Task<Region> DeleteAsync(Guid id);
+        Task<Region> UpdateAsync(Guid id, Region region);
     }
 }
